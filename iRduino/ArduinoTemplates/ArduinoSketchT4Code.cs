c23c6388ea3543f5cd0d499a9f259d79aa5ec2e5
Mark Silverwood <msilverwood@gmail.com>
2013-02-14 13:28:36 +0200

    Arduino Sketch Generation Improvements
    Fixes #43
    Arduino Sketch now generated using T4 templates and when saving sketches
    it will now ensure that the folder is correct and if not then create it
    for you.

---
+++ iRduino/ArduinoTemplates/ArduinoSketchT4Code.cs
@@ -0,0 +1,18 @@
+﻿//========================================//
+// iRduino - Created by Mark Silverwood  //
+//======================================//
+namespace iRduino.ArduinoTemplates
+{
+    using iRduino.Classes;
+
+    partial class ArduinoSketchT4
+    {
+        private readonly ConfigurationOptions configurationOptions;
+        private readonly ArduinoPins pins;
+        public ArduinoSketchT4(ConfigurationOptions configurationOptions, ArduinoPins pins)
+        {
+            this.configurationOptions = configurationOptions;
+            this.pins = pins;
+        }
+    }
+}
